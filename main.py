1ec0fd03069ee6b01a254733a65a5cbb258bfd3c
KajahSabesh <21185@students.mrgs.school.nz>
2024-05-15 14:26:38 +1200

    added hello world and bye world to window.

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ window = Tk()
 window.geometry("300x300")
 window.title("My App")
 
+window.grid()
+
 hello_label = Label(text="Hello world")
-hello_label.pack(side="left")
+hello_label.grid(column=1, row=1)
+
+bye_label = Label(text="Bye, World")
+bye_label.grid(column=1, row=1)
 
 window.mainloop()
