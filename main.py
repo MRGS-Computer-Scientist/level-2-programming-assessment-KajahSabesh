4167ee0ce0a58be4f783bf1931d2f0c293174472
KajahSabesh <21185@students.mrgs.school.nz>
2024-05-22 14:58:19 +1200

    added a bit of code.

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from tkinter import *
 
+w_width = 500
+w_height = 700
+
 window = Tk()
-window.geometry("300x300")
-window.title("My App")
+window.geometry(str(w_width)+ "x" + str(w_height))
+window.title("Dieting Daddy")
 
-window.grid()
+top_frame = Frame (background= 'red', width=w_width, height=100)
+top_frame.pack()
 
-hello_label = Label(text="Hello world")
-hello_label.grid(column=1, row=1)
+main_frame = Frame(background =bg_color, width=w_width, height=(w_height=200))
 
-bye_label = Label(text="Bye, World")
-bye_label.grid(column=1, row=1)
-
-window.mainloop()
