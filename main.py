a3ac346de46ab1d70eea9b96024ad0327f2990fd
KajahSabesh <21185@students.mrgs.school.nz>
2024-05-24 12:21:49 +1200

    added app.py

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-from tkinter import *
+from App import *
 
-w_width = 500
-w_height = 700
-
-window = Tk()
-window.geometry(str(w_width)+ "x" + str(w_height))
-window.title("Dieting Daddy")
-
-top_frame = Frame(background= 'orange', width=w_width, height=100)
-top_frame.pack()
-
-main_frame = Frame(background= 'white', width=w_width, height=100)
-main_frame.pack()
-
-bottom_frame = Frame(background= 'green', width=w_width, height=100)
-bottom_frame.pack()
-
-home_button = Button(bottom_frame, text="Home", height=5, width=5, bg='green')
-home_button.place(x=0,y=2)
-
-window.mainloop()
+if __name__ == "__main__":
+    app = App()
+    
