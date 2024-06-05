cfd245fb045e131bc903654b1e91d9a71e57eba4
KajahSabesh <21185@students.mrgs.school.nz>
2024-06-05 14:37:58 +1200

    changed the whole backgorund to black

--- App.py
+++ App.py
@@ -9,8 +9,9 @@ class App():
         self.window.geometry(str(w_width)+ "x" + str(w_height))
         self.window.title("My App")
         
+        self.window.configure(background="black")
         
-        self.bottom_frame = Frame(background='black', width=150, height=1000)
+        self.bottom_frame = Frame(background='black', width=500, height=1000)
         self.bottom_frame.pack(side='left')
 
         self.home_button = Button(self.bottom_frame, text="Home", height=2, width=10, bg='red')
@@ -20,13 +21,13 @@ class App():
         self.account_button.place(x=40,y=50)
         
         self.calander_button = Button(self.bottom_frame, text="Calendar", height=2, width=10, bg='red')
-        self.account_button.place(x=40,y=200)
+        self.calander_button.place(x=40,y=150)
         
-        self.shop_button = Button(self.button_frame, text='Shop', height=2, width=10, bg='red')
-        self.shop_button.place(x=40,y=250)
+        self.shop_button = Button(self.bottom_frame, text='Shop', height=2, width=10, bg='red')
+        self.shop_button.place(x=40,y=200)
         
         self.exit_button = Button(self.bottom_frame, text="Exit", height=2, width=10, bg='red')
-        self.exit_button.place(x=40,y=300)
+        self.exit_button.place(x=40,y=250)
         
         self.dirname = path.dirname(__file__)
         self.filename = path.join(self.dirname, 'images/')
