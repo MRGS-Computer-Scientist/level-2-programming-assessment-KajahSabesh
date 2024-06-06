1a9768dd0f1994552ccf4f5c377a544685e63937
KajahSabesh <21185@students.mrgs.school.nz>
2024-06-06 13:18:05 +1200

    added activity and calories burnt boxes.

--- App.py
+++ App.py
@@ -13,21 +13,35 @@ class App():
         
         self.bottom_frame = Frame(background='black', width=500, height=1000)
         self.bottom_frame.pack(side='left')
+        
+        button_font = ("Helvita", 12, "bold") 
 
-        self.home_button = Button(self.bottom_frame, text="Home", height=2, width=10, bg='red')
-        self.home_button.place(x=40,y=100)
+        self.home_button = Button(self.bottom_frame, text="Home", height=3, width=15, bg='red', font=button_font)
+        self.home_button.place(x=20,y=200)
        
-        self.account_button = Button(self.bottom_frame, text="Account", height=2, width=10, bg='red')
-        self.account_button.place(x=40,y=50)
+        self.account_button = Button(self.bottom_frame, text="Account", height=3, width=15, bg='red', font=button_font)
+        self.account_button.place(x=20,y=100)
         
-        self.calander_button = Button(self.bottom_frame, text="Calendar", height=2, width=10, bg='red')
-        self.calander_button.place(x=40,y=150)
+        self.calander_button = Button(self.bottom_frame, text="Calendar", height=3, width=15, bg='red', font=button_font)
+        self.calander_button.place(x=20,y=300)
         
-        self.shop_button = Button(self.bottom_frame, text='Shop', height=2, width=10, bg='red')
-        self.shop_button.place(x=40,y=200)
+        self.shop_button = Button(self.bottom_frame, text='Shop', height=3, width=15, bg='red', font=button_font)
+        self.shop_button.place(x=20,y=400)
         
-        self.exit_button = Button(self.bottom_frame, text="Exit", height=2, width=10, bg='red')
-        self.exit_button.place(x=40,y=250)
+        self.statistics_button = Button(self.bottom_frame, text='Statistics', height=3, width=15, bg='red', font=button_font)
+        self.statistics_button.place(x=20, y=500)
+        
+        self.exit_button = Button(self.bottom_frame, text="Exit", height=3, width=15, bg='red', font=button_font)
+        self.exit_button.place(x=20,y=600)
+        
+        self.space_label = Label(self.window, bg='black', width=20, height=5)
+        self.space_label.pack()
+        
+        self.activity_box = Label(self.window, text="Activities", bg='red', width=20, height=10)
+        self.activity_box.pack()
+        
+        self.caloriesburnt_box = Label(self.window, text="Calories Burnt", bg='red', width=20, height=10)
+        self.caloriesburnt_box.pack()
         
         self.dirname = path.dirname(__file__)
         self.filename = path.join(self.dirname, 'images/')
